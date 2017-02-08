cc1bc1b814f4eb34802c187194a1bdfce42429b0
amertis <amerths@gmail.com>
2017-02-08 11:51:37 +0200

    removed encoding

--- json2.js
+++ json2.js
@@ -218,13 +218,14 @@
 // Otherwise we must also replace the offending characters with safe escape
 // sequences.
 
-        escapable.lastIndex = 0;
-        return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
-            var c = meta[a];
-            return typeof c === 'string'
-                ? c
-                : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-        }) + '"' : '"' + string + '"';
+        //escapable.lastIndex = 0;
+        //return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
+        //    var c = meta[a];
+        //    return typeof c === 'string'
+        //        ? c
+        //        : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+        //}) + '"' : '"' + string + '"';
+        return '"' + string + '"';
     }
 
 
